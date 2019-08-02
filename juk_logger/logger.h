5e0684f7f987af6aa030b7a1b67c94633d83e54c
Mikhail Slv <mikhail.slv@yandex.ru>
2019-08-02 10:37:53 +0300

    upd

--- juk_logger/logger.h
+++ juk_logger/logger.h
@@ -9,30 +9,33 @@ template<typename T>
 	class SimpleSub
 	{
 	public:
-		SimpleSub(ros::NodeHandle* nh,std::string topic_name):
-			name(topic_name)
+		SimpleSub(ros::NodeHandle* nh, std::string topic_name_, ros::Time start_time_)
+			:
+			topic_name(topic_name_),
+			start_time(start_time_)
 		{
 			sub = nh->subscribe(topic_name, 1, &SimpleSub::callback, this);
 			upd_time = ros::Time::now();
 		}
 		
-		std::string name;
+		std::string topic_name;
 		T data;
 		ros::Time upd_time;
+		ros::Time start_time;
 		ros::Subscriber sub;
 		
 		std::stringstream full_str;
 		
 		std::string get_full_str()
 		{
-			return full_str.str() +  "$TOPIC\n\n";
+			return full_str.str();
 		}
 		void reset_str()
 		{
 			full_str = std::stringstream();
 			full_str.flags(std::ios::fixed);
 			full_str.precision(10);
-			full_str << "@TOPIC:[" << name <<"]"<< std::endl;
+			full_str << "@TOPIC:[" << topic_name <<"]"<< std::endl;
 			full_str << "\tLAST_UPD:[" << upd_time << "]" << std::endl;
 		}
 		template<typename V>
