e4a6c4a3a5097f6133a0b24bf1400aba45d1bd9f
SMP33 <ubuntu@ubuntu.mshome.net>
2019-08-05 14:01:35 +0000

    upd

--- install_commands.sh
+++ install_commands.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cp juk_launch /usr/local/bin/
-cp juk_stop /usr/local/bin/
+sudo cp juk_launch /usr/local/bin/
+sudo cp juk_stop /usr/local/bin/
 
-cp juk_launch /usr/bin/
-cp juk_stop /usr/bin/
+sudo cp -r build/bin/* /usr/local/bin/
