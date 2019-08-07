b0fb5a1c81af44879ba776f34305d9de1ab3f8d4
Mikhail Slv <mikhail.slv@yandex.ru>
2019-08-07 22:15:27 +0300

    upd

--- install_commands.sh
+++ install_commands.sh
@@ -3,4 +3,5 @@
 sudo cp juk_launch /usr/local/bin/
 sudo cp juk_stop /usr/local/bin/
 
-sudo cp -r build/bin/* /usr/local/bin/
+sudo mkdir /usr/local/bin/juk/
+sudo cp -r build/bin/* /usr/local/bin/juk/
