6a175cced5704d64d351103ff06f104a50532567
SMP33 <mikhail.slv@yandex.ru>
2019-09-25 12:40:07 +0100

    upd

--- include/ArgParser.h
+++ include/ArgParser.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#ifndef __ARG_PARSER__
+#define __ARG_PARSER__
+
 #include <iostream>
 #include <map>  
 #include <vector>
@@ -10,65 +13,14 @@ class ArgParser_Int
 public:
 	ArgParser_Int();
 	std::map< const char*, int> args;
-	void add(const char* name, int base_value=0);
-	void add(std::pair<const char*,int> arg);
-	void add(std::map<const char*,int> arg);
+	void add(const char* name, int base_value = 0);
+	void add(std::pair<const char*, int> arg);
+	void add(std::map<const char*, int> arg);
 	void parse(std::string str);
-	void parse(int argc,char** argv);
+	void parse(int argc, char** argv);
 
 
 };
 
-#include "ArgParser.h"
 
-ArgParser_Int::ArgParser_Int() :
-	args()
-{
-}
-
-
-
-void ArgParser_Int::add(const char* name, int base_value)
-{
-	args[name] = base_value;
-}
-
-void ArgParser_Int::add(std::pair<const char*, int> arg)
-{
-	
-}
-
-void ArgParser_Int::add(std::map<const char*, int> arg_map)
-{
-	for (auto arg : arg_map)
-	{
-		args[arg.first] = arg.second;
-	}
-}
-
-void ArgParser_Int::parse(std::string str)
-{
-	for (auto& elem : args)
-	{
-		std::string needle = std::string(elem.first) + "=";
-
-		int sl = str.length(), nl = needle.length();
-
-		int pos_find = (int)str.find(needle);
-
-		if (pos_find == 0 && sl > nl)
-		{
-			elem.second = std::stoi(str.substr(nl, sl - nl));
-		}
-	}
-
-}
-
-void ArgParser_Int::parse(int argc,char** argv)
-{
-	for (int i = 0; i < argc; i++)
-	{
-		std::string str = argv[i];
-		parse(str);
-	}
-}
+#endif // !__ARG_PARSER__
